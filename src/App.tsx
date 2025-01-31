cd46723f6171f311b7db4f540fde347ffc34da81
Anmol <anmoliiita123@gmail.com>
2025-02-01 01:17:00 +0530

    Changes: Project initiated & setup completed with shadCn UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+function App() {
+  return (
+    <div className='text-red-500'>
+      <Button>Click Me</Button>
+    </div>
+  );
+  
+};
+
+export default App
